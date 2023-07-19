6973885b741237777457e6335dbfa2b2196f7373
Jordano <jgtomazoni@ucs.br>
2023-07-19 13:39:49 -0300

    WIP - Banco criado e populado com uso de migrations, seeders e factory;

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Video>
+ */
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'titulo' => fake()->name(),
+            'descricao' => Str::random(10),
+            'url' => Str::random(10),
+        ];
+    }
+}
