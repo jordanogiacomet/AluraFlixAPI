45fdaf60874a1173ecdf4b0da3069754af7639c8
Jordano <jgtomazoni@ucs.br>
2023-07-19 14:02:13 -0300

    WIP - Operações HTTP get implementadas;

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $videos = Video::all();
+
+        return response()->json($videos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $video = DB::table('videos')
+                        ->where('id', $id)
+                        ->get();
+
+        if($video->isEmpty()){
+            return response()->json([
+                'message' => 'Não encontrado.'
+            ]);
+        }
+
+            return response()->json($video);
+        }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
