91ea7816b52d6d594729de87614048d24b85d0b0
Jordano <jgtomazoni@ucs.br>
2023-07-19 18:38:49 -0300

    WIP - Banco criado e populado com uso de migrations, seeders e factory;

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call(VideoSeeder::class);
+        $this->call(CategorySeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
