f7af63c912fdf67b276f54261a9fd19b35b75ce8
Jordano <jgtomazoni@ucs.br>
2023-07-20 21:07:01 -0300

    WIP - Testes de unidades para os modelos implementados;

--- tests/Unit/CategoryModelTest.php
+++ tests/Unit/CategoryModelTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Unit;
 
+use Tests\TestCase;
+use App\Models\Video;
 use App\Models\Category;
-use PHPUnit\Framework\TestCase;
 
 class CategoryModelTest extends TestCase
 {
@@ -17,6 +18,34 @@ class CategoryModelTest extends TestCase
 
         $this->assertInstanceOf(Category::class, $category);
         $this->assertDatabaseHas('categories', $data);
+
+
+    }
+
+    public function testRetrieveCategory(){
+        $category = Category::factory()->create();
+
+        $retrievedCategory = Category::find($category->id);
+
+        $this->assertEquals($category->id, $retrievedCategory->id);
+
+    }
+
+    public function testCategoryVideosRelationship(){
+        $category = Category::factory()->create();
+
+        $videos = Video::factory()->count(3)->create([
+            'categoriaId' => $category->id
+        ]);
+    
+        $retrievedVideos = $category->videos;
+    
+        
+        $this->assertCount(3, $retrievedVideos);
+
+        foreach($videos as $video){
+            $this->assertTrue($retrievedVideos->contains($video));
+        }
 
 
     }
--- tests/Unit/VideoModelTest.php
+++ tests/Unit/VideoModelTest.php
@@ -2,15 +2,38 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Video;
+use App\Models\Category;
+use Tests\TestCase;
 
 class VideoModelTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
+
+    public function testCreateVideo(){
+        
+        $category = Category::factory()->create();
+        
+        $data = [
+            'categoriaId' => $category->id,
+            'titulo' => 'Video de teste',
+            'descricao' => 'Descricao de teste',
+            'url' => 'https://www.youtube.com/watch?v=bgqGdIoa52s'
+        ];
+    
+        $video = Video::create($data);
+
+        $this->assertInstanceOf(Video::class, $video);
+        $this->assertDatabaseHas('videos', $data);
+    
     }
+
+
+    public function testRetrievedVideo(){
+        $video = Video::factory()->create();
+
+        $retrievedVideo = Video::find($video->id);
+
+        $this->assertEquals($video->id, $retrievedVideo->id);
+    }
+
 }
